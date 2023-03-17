c6fe16e84a243bc85fa7354ef2cc672d58b5f1f0
pepper11com <pepperherman3@gmail.com>
2023-03-17 23:45:08 +0100

    kinda working

--- app/src/main/java/com/example/mat3_nav/ui/screens/AppHomeScreen.kt
+++ app/src/main/java/com/example/mat3_nav/ui/screens/AppHomeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -31,141 +32,135 @@ import kotlinx.coroutines.launch
 fun AppHomeScreen(
     navController: NavController
 ) {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
 
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-    val skipPartiallyExpanded by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
-    val bottomSheetState = rememberModalBottomSheetState(
-        skipPartiallyExpanded = skipPartiallyExpanded
-    )
+    val scaffoldState = rememberBottomSheetScaffoldState()
 
-    Scaffold(
-        bottomBar = {
-            BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.surface,
-                contentColor = MaterialTheme.colorScheme.onSurface,
-                tonalElevation = 8.dp,
-                contentPadding = PaddingValues(16.dp)
-            ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = 128.dp,
+            sheetContent = {
                 Box(
                     Modifier
-                        .weight(1f)
-                        .padding(end = 16.dp)
+                        .fillMaxWidth()
+                        .padding(bottom = 56.dp)
+                        .height(128.dp),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Surface(
-                        shape = RoundedCornerShape(16.dp),
-                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.Search,
-                                contentDescription = "Search",
-                                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                                modifier = Modifier.size(18.dp)
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            TextField(
-                                value = "",
-                                onValueChange = {},
-                                modifier = Modifier.fillMaxWidth(),
-                                placeholder = { Text(text = "Search") },
-                                textStyle = MaterialTheme.typography.titleMedium,
-                                singleLine = true,
-                                colors = TextFieldDefaults.textFieldColors(
-                                    containerColor = Color.Transparent,
-                                    cursorColor = MaterialTheme.colorScheme.primary,
-                                    focusedIndicatorColor = Color.Transparent,
-                                    unfocusedIndicatorColor = Color.Transparent
-                                )
-                            )
+                    Text("Swipe up to expand sheet")
+                }
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(64.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("Sheet content")
+                    Spacer(Modifier.height(20.dp))
+                    Button(
+                        onClick = {
+                            scope.launch { scaffoldState.bottomSheetState.partialExpand() }
                         }
+                    ) {
+                        Text("Click to collapse sheet")
                     }
                 }
-
-                Surface(
-                    shape = CircleShape,
-                    color = MaterialTheme.colorScheme.primary,
-                    onClick = { /* Handle right button click */ },
+            }) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .zIndex(10f),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                NavigationBar(
                     modifier = Modifier
-                        .size(48.dp)
-                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .height(84.dp),
                 ) {
-                    IconButton(
-                        onClick = { openBottomSheet = true },
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Add"
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                            label = { Text(item) },
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
                         )
                     }
                 }
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Home",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "This is the home screen",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            scope.launch { scaffoldState.bottomSheetState.expand() }
+                        }
+                    ) {
+                        Text("Click to expand sheet")
+                    }
+                }
             }
         }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            BottomModal(
-                openBottomSheet = openBottomSheet,
-                onDismissRequest = { openBottomSheet = false },
-                bottomSheetState = bottomSheetState,
-                scope = scope
-            )
-            // Your screen content here
-        }
+
+
+//            DismissibleDrawerSheet(
+//                //    modifier: Modifier,
+//                //    drawerShape: Shape,
+//                //    drawerContainerColor: Color,
+//                //    drawerContentColor: Color,
+//                //    drawerTonalElevation: Dp,
+//                //    windowInsets: WindowInsets,
+//                //    content: @Composable() (ColumnScope.() -> Unit)
+//
+//            ){
+//                Text("Hello")
+//            }
+
+
     }
+
+
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun BottomModal(
-    openBottomSheet: Boolean,
-    onDismissRequest: () -> Unit,
-    bottomSheetState: SheetState,
-    scope: CoroutineScope
-) {
-    if (openBottomSheet) {
-        ModalBottomSheet(
-            onDismissRequest = onDismissRequest,
-            sheetState = bottomSheetState,
-        ) {
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Button(
-                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
-                    // you must additionally handle intended state cleanup, if any.
-                    onClick = {
-                        scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
-                            if (!bottomSheetState.isVisible) {
-                                onDismissRequest.invoke()
-                            }
-                        }
-                    }
-                ) {
-                    Text("Hide Bottom Sheet")
-                }
-            }
-            LazyColumn {
-                items(50) {
-                    ListItem(
-                        headlineContent = { Text("Item $it") },
-                        leadingContent = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        }
-                    )
-                }
-            }
-        }
-    }
-}
+//    Scaffold(
+//        bottomBar = {
+//
+//
+//
+//
+//        },
+//        content = { innerPadding ->
+//            LazyColumn(
+//                modifier = Modifier
+//                    .padding(innerPadding)
+//            ) {
+//                items(100) {
+//                    Text("Item $it", modifier = Modifier.padding(16.dp))
+//                }
+//            }
+//        }
+//    )
+
+
 
 
